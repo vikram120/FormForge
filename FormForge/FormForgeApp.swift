ef83a6f1dd9147b8f9cee315428a4c6228f0aaee
vikram <vikramkunwar1404@gmail.com>
2026-05-26 14:44:12 +0530

    Initial Commit

---
+++ FormForge/FormForgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormForgeApp.swift
+//  FormForge
+//
+//  Created by Vikram Kunwar on 26/05/26.
+//
+
+import SwiftUI
+
+@main
+struct FormForgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
